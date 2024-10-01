7e617ff39a1658a3188281fc7421ae3dbd2aa28b
Annylory LR <annyloryrosa@gmail.com>
2024-10-01 18:51:50 -0300

    adding java: sidebar and sending first message.

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("linked!")
+
+let userName= prompt('Por favor, insira um nome:')
+let recipient= prompt('Quem pode ver essa mensagem?')
+
+      
+
+
+function accessSidebar(){
+    element = document.querySelector(".sidebar");
+    element.classList.remove('hidden');
+    shadow = document.querySelector(".shadow")
+    shadow.classList.remove("clear")
+
+}
+
+function hideSidebar(){
+    element = document.querySelector(".sidebar");
+    element.classList.add('hidden');
+    shadow = document.querySelector(".shadow")
+    shadow.classList.add("clear")
+
+}
+
+function message(){
+    let getMessage = document.querySelector('.textarea')
+    let message = getMessage.value
+    let messageList = document.querySelector('ul')
+    
+
+      messageList.innerHTML+=
+    ` <li class="white-box chat">
+        <p class="message"><time datetime="2024-10-01 15:01:00">(15:01:00)</time> <b>${userName}</b> para <b>${recipient}</b>: ${message}!</p>
+      </li>`
+
+}
+
