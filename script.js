0519b5dde82fd144021e0e1c87186da8974dc1d9
Annylory LR <annyloryrosa@gmail.com>
2024-10-03 20:36:47 -0300

    message type | message mode | changing contact and message mode

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 
+let chat = [{from:"João", to: "Todos", text:"entra na sala...", type:"status", time:"08:01:50"},
+            {from:"João", to: "Todos", text:"Bom dia", type:"message", time:"08:02:50"},
+            {from:"Maria", to: "João", text:"Oi João :)",type:"private_message", time:"08:03:50" },
+            {from:"João", to: "Maria", text:"Oi gatinha quer tc?", type:"private_message", time:"08:04:50"},
+            {from:"Maria", to: "Todos", text:"sai da sala...", type:"status", time:"08:04:50"}
+]
+
+let users = [{name:"Todos"},{name:"João"},{name:"Maria"}]
+
+let roomID = "5c28a11f-e350-4b3f-97dd-ddeb552a1465"
 let userName= prompt('Por favor, insira um nome:')
 let recipientName= "Todos"
 let messageMode="Público"
@@ -22,39 +32,88 @@ function hideSidebar(){
 
 }
 
-function message(){
-    let getMessage = document.querySelector('.textarea')
-    let message = getMessage.value
-    let messageList = document.querySelector('ul')
-        
-    
-    if(messageMode === 'Público' && recipientName !== ""){
-      messageList.innerHTML+=
-      ` <li class="white-box chat">
-          <p class="message"><time datetime="2024-10-01 15:01:00">(15:01:00)</time> <b>${userName}</b> para <b>${recipientName}</b>: ${message}</p>
-        </li>`
-  
-      document.querySelector('.textarea').value = ""
-  
+function addUser(){
+  let usersList = document.querySelector('.userList');
+  usersList.innerHTML = ""
+  users.push({name:userName});
 
-    } else if(messageMode === 'Reservadamente' && recipientName !== "" && recipientName !== 'Todos'){
 
-      messageList.innerHTML+=
-      `<li class="red-box private">
-        <p class="message"><time datetime="2024-10-01 15:01:00">(15:01:00)</time> <b class="name">${userName}</b> reservadamente para <b class="recipient">${recipientName}</b>: ${message}</p> 
+  for(let index=0; index < users.length; index++){
+
+    usersList.innerHTML+=
+      `<li class="options" onclick="recipient(this); statusText()">
+        <ion-icon class="svg" name="people"></ion-icon>
+        <p class="name">${users[index].name}</p>
+        <svg class="mark" width="9" height="7" margin viewBox="0 0 13 11" fill="none"           xmlns="http://www.w3.org/2000/svg">
+          <path d="M11 2L4.7 9L2 6.375" stroke="#28BB25" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>   
       </li>`
-      document.querySelector('.textarea').value = ""
-     
-    } else{ 
-      alert("Por favor, escolha um Contato e a Visibilidade")
-      document.querySelector('.textarea').value = ""
+
+    
+  }
+
+}
+
+function getMessage(){
+ 
+  let getMessage = document.querySelector('.textarea');
+  let message = getMessage.value;
+  let type = "";
+  
+  if(messageMode === 'Público'){
+    type = "message"
+    chat.push({from:userName, to: recipientName, text:message, type:type, time:"08:04:50"})
+
+  } else{
+
+    type = "private_message"
+
+    chat.push({from:userName, to: recipientName, text:message, type:type, time:"08:04:50"})
+
+  }
+
+  document.querySelector('.textarea').value = ""
+
+  messageRender()
+
+
+}
+
+function messageRender(){
+    
+    let messageList = document.querySelector('.messages');
+    messageList.innerHTML = "";
+    for(let i=0; i < chat.length; i++){
+
+      if(chat[i].type === "message"){
+        messageList.innerHTML+=
+        ` <li class="white-box chat">
+            <p class="message"><time datetime="2024-10-01 15:01:00">(15:01:00)</time> <b>${chat[i].from}</b> para <b>${chat[i].to}</b>: ${chat[i].text}</p>
+          </li>`
+       
+  
+      } else if(chat[i].type === "private_message"){
+  
+        messageList.innerHTML+=
+        `<li class="red-box private">
+          <p class="message"><time datetime="2024-10-01 15:01:00">(15:01:00)</time> <b class="name">${chat[i].from}</b> reservadamente para <b class="recipient">${chat[i].to}</b>: ${chat[i].text}</p> 
+        </li>`
+      
+
+      } else{
+        messageList.innerHTML+=
+        `<li class="gray-box statusMessage">
+          <p class="message"><time datetime="2024-10-01 15:01:00">(15:01:00)</time> <b class="name">${chat[i].from}</b> ${chat[i].text}</p> 
+        </li>`
+
+
+      }
 
     }
-
-    console.log(recipientName)
-    console.log(messageMode)
-    listRefresh()
-  //*******Para atualizar a lista: console.log(messageList.lastChild)
+    
+        
+    //listRefresh()
+    
    
 }
 
@@ -134,15 +193,9 @@ function statusText(){
 
 function listRefresh(){
 
-  /// reload the page a cada 3s
-  /// a cada 5s atualizar status de quem esta na sala
-
-  /// manter a ultima mensagem visivel
-  
   let messageList = document.querySelector('ul');
   let lastMessage = messageList.lastChild
   lastMessage.scrollIntoView();
-
 
 }
 
@@ -150,3 +203,5 @@ function reloading() {
   location.reload();
 }
 
+messageRender()
+addUser()
